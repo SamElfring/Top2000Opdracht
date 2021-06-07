eac5390e96dbca5f5ccb6dbd588e2db22420b7b5
Sam <elfringsam@gmail.com>
2021-06-07 14:41:19 +0200

    User en Role controllers toegevoegd

---
+++ Top2000/Top2000/Controllers/RoleController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Top2000.Models;
+
+namespace Top2000.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly db_a74225_top2000Context _context;
+
+        public RoleController(db_a74225_top2000Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Role
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AspNetRoles.ToListAsync());
+        }
+
+        // GET: Role/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetRole = await _context.AspNetRoles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aspNetRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetRole);
+        }
+
+        // GET: Role/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Role/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,NormalizedName,ConcurrencyStamp")] AspNetRole aspNetRole)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aspNetRole);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aspNetRole);
+        }
+
+        // GET: Role/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetRole = await _context.AspNetRoles.FindAsync(id);
+            if (aspNetRole == null)
+            {
+                return NotFound();
+            }
+            return View(aspNetRole);
+        }
+
+        // POST: Role/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,NormalizedName,ConcurrencyStamp")] AspNetRole aspNetRole)
+        {
+            if (id != aspNetRole.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aspNetRole);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AspNetRoleExists(aspNetRole.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aspNetRole);
+        }
+
+        // GET: Role/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetRole = await _context.AspNetRoles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aspNetRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetRole);
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var aspNetRole = await _context.AspNetRoles.FindAsync(id);
+            _context.AspNetRoles.Remove(aspNetRole);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AspNetRoleExists(string id)
+        {
+            return _context.AspNetRoles.Any(e => e.Id == id);
+        }
+    }
+}
