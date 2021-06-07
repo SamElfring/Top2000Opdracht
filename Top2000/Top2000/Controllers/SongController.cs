2225ec9ae7f129c083b0608be62289aa89ef0023
Sam <elfringsam@gmail.com>
2021-06-07 09:42:41 +0200

    Project geupdate

---
+++ Top2000/Top2000/Controllers/SongController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Top2000.Models;
+
+namespace Top2000.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            Song song = new Song
+            {
+                Songid = 0,
+                Titel = "Hallo",
+                Artiestid = 5656,
+                Jaar = 2006
+            };
+
+            return View(song);
+        }
+    }
+}
---
+++ Top2000/Top2000/Controllers/Top2000Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Top2000.Models;
+
+namespace Top2000.Controllers
+{
+    public class Top2000Controller : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
