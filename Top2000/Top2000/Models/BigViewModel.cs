75c483be97907b6652219fcb5bc6e10afdbf6d2b
Sam <elfringsam@gmail.com>
2021-06-14 12:34:08 +0200

    Account Area kun je nu bewerken

---
+++ Top2000/Top2000/Models/BigViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Top2000.Models
+{
+    public class BigViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public Lijst Lijst { get; set; }
+        public Song Song { get; set; }
+    }
+}
